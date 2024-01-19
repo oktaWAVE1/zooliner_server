a2f43ef08f6754332e8c31c77a4f962679a1d557
Sergey Kosygin <c.v.k@mail.ru>
2024-01-19 15:10:33 +0300

    error logging

--- error/ApiError.js
+++ error/ApiError.js
@@ -9,16 +9,18 @@ class ApiError extends Error {
         this.message = message
     }
     static async badRequest (message) {
+        console.log(__dirname)
         console.log(message)
         await fs.appendFile(`../server/logs/logs.txt`, `${timeStampLog} Ошибка: ${message} \n`, (err) => {
             if (err) {
-                console.log(err);
+                console.log(404, err);
             }
         })
         return new ApiError(404, message)
     }
     static async internal (message) {
-        console.log(message)
+        console.log(__dirname)
+        console.log(500, message)
         await fs.appendFile(`../server/logs/logs.txt`, `${timeStampLog} Ошибка: ${message} \n`, (err) => {
             if (err) {
                 console.log(err);
@@ -27,7 +29,8 @@ class ApiError extends Error {
         return new ApiError(500, message)
     }
     static async forbidden (message) {
-        console.log(message)
+        console.log(__dirname)
+        console.log(403, message)
         await fs.appendFile(`../server/logs/logs.txt`, `${timeStampLog} Ошибка: ${message} \n`, (err) => {
             if (err) {
                 console.log(err);
