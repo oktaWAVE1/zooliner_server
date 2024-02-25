7e13b77ada06b74f06daa86fe8e20c9b87356df0
Sergey Kosygin <c.v.k@mail.ru>
2024-02-25 18:38:45 +0300

    yandex feed generator fix

--- service/yandex-feed-generator.js
+++ service/yandex-feed-generator.js
@@ -25,7 +25,7 @@ async function yandexFeedGenerator () {
 %categories%
         </categories>
         <offers>
-            <offer id="9012">
+           
 %offers%                                           
                
             </offer>
